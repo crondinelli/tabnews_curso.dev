b6d92ae005579ed61a06647db61884f3148e48cd
crondinelli@gmail.com <crondinelli@gmail.com>
2025-10-22 14:29:59 -0300

    alterei o texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste</h1>
+    return <h1>Oi amor, se vc abrir corretamente este link, vai ver a mensagem...  Te amo muito, ta? Se ver e gostar me dá um beijinho!!!</h1>
 }
 
 export default Home;
