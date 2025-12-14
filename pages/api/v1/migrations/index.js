83ded13c4db7d47a3316cdf2e3cb78d8be7eb86a
crondinelli@gmail.com <crondinelli@gmail.com>
2025-12-14 12:33:45 -0300

    add `/api/v1/migratios` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,14 +1,37 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
+  const dbClient = await database.getNewClient();
+  const defaulMigrationsOptions = {
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
-  });
-  response.status(200).json(migrations);
+    verbose: true,
+  };
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaulMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaulMigrationsOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
 }
 
 // async para tornar a função asincrona
